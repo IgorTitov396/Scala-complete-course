94aebfc46e55106dd4455a4e7429c7e7794ac8c9
Igor Titov <i.titov1@tinkoff.ru>
2017-10-20 18:56:49 +0300

    Homework #5 v0.1.0 Implements add and find

--- src/main/scala/lectures/oop/BST.scala
+++ src/main/scala/lectures/oop/BST.scala
@@ -1,5 +1,7 @@
 package lectures.oop
 
+import scala.util.Random
+
 
 /**
   * BSTImpl - это бинарное дерево поиска, содержащее только значения типа Int
@@ -30,18 +32,39 @@ trait BST {
   def add(newValue: Int): BST
 
   def find(value: Int): Option[BST]
+
+  def myToString():String
 }
 
 case class BSTImpl(value: Int,
                    left: Option[BSTImpl] = None,
                    right: Option[BSTImpl] = None) extends BST {
 
-  def add(newValue: Int): BST = ???
+  def add(newValue: Int): BST = {
+    if (newValue < value) {
+      if (left.isDefined) this.copy(left = Option(left.get.add(newValue).asInstanceOf[BSTImpl])) else this.copy(left = Option(BSTImpl(newValue)))
+    }
+    else {
+      if (right.isDefined) this.copy(right = Option(right.get.add(newValue).asInstanceOf[BSTImpl])) else this.copy(right = Option(BSTImpl(newValue)))
+    }
+  }
 
-  def find(value: Int): Option[BST] = ???
+  def find(value: Int): Option[BST] = {
+    if (value == this.value) Option(this)
+    else if (value > this.value) right
+    else left
+  }
 
-  // override def toString() = ???
-
+  override def myToString() = {
+    def depthOfBST(acc: Int, bst: BST): Int = bst match {
+      case BSTImpl(_, left, right) if left.isDefined && right.isDefined => math.max(depthOfBST(acc + 1, left.get), depthOfBST(acc + 1, right.get))
+      case BSTImpl(_, left, _) if left.isDefined => depthOfBST(acc + 1, left.get)
+      case BSTImpl(_, _, right) if right.isDefined =>  depthOfBST(acc + 1, right.get)
+      case _ => acc
+    }
+    depthOfBST(1, this).toString
+  }
+  //override def toString()
 }
 
 object TreeTest extends App {
@@ -55,9 +78,16 @@ object TreeTest extends App {
   val markerItem3 = (Math.random() * maxValue).toInt
 
   // Generate huge tree
-  val root: BST = BSTImpl(maxValue / 2)
-  val tree: BST = ??? // generator goes here
+  def generatror(acc: Int, bstTree: BST): BST = acc match {
+    case value if value == nodesCount => bstTree.add(Random.nextInt(maxValue))
+    case _ => generatror(acc + 1, bstTree.add(Random.nextInt(maxValue)))
+  }
 
+  val root: BST = BSTImpl(maxValue / 2)
+  val tree: BST = generatror(0, root)
+
+  println(tree.myToString())
+  println(tree)
   // add marker items
   val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
 
