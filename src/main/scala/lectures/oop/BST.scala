40c3c7aa72d73b7aa033808ed7146202d0ba5a3a
Igor Titov <i.titov1@tinkoff.ru>
2017-10-30 17:16:44 +0300

    Homework #5 v1.0.0 Implements toString

--- src/main/scala/lectures/oop/BST.scala
+++ src/main/scala/lectures/oop/BST.scala
@@ -32,8 +32,6 @@ trait BST {
   def add(newValue: Int): BST
 
   def find(value: Int): Option[BST]
-
-  def myToString():String
 }
 
 case class BSTImpl(value: Int,
@@ -55,16 +53,71 @@ case class BSTImpl(value: Int,
     else left
   }
 
-  override def myToString() = {
-    def depthOfBST(acc: Int, bst: BST): Int = bst match {
-      case BSTImpl(_, left, right) if left.isDefined && right.isDefined => math.max(depthOfBST(acc + 1, left.get), depthOfBST(acc + 1, right.get))
-      case BSTImpl(_, left, _) if left.isDefined => depthOfBST(acc + 1, left.get)
-      case BSTImpl(_, _, right) if right.isDefined =>  depthOfBST(acc + 1, right.get)
-      case _ => acc
+  override def toString: String = {
+    def depthOfBSTAndMaxValue(acc: Int, bst: BST): (Int, Int) = bst match {
+      case BSTImpl(_, leftVal, rightVal) if leftVal.isDefined && rightVal.isDefined =>
+        val (leftRes, rightRes) = (depthOfBSTAndMaxValue(acc + 1, leftVal.get), depthOfBSTAndMaxValue(acc + 1, rightVal.get))
+        (math.max(leftRes._1, rightRes._1), math.max(leftRes._2, rightRes._2))
+      case BSTImpl(_, leftVal, _) if leftVal.isDefined => depthOfBSTAndMaxValue(acc + 1, leftVal.get)
+      case BSTImpl(_, _, rightVal) if rightVal.isDefined =>  depthOfBSTAndMaxValue(acc + 1, rightVal.get)
+      case _ => (acc, bst.value)
     }
-    depthOfBST(1, this).toString
+    val (depth, maxValue) = depthOfBSTAndMaxValue(1, this)
+
+    val maxLength = maxValue.toString.toCharArray.length
+
+    def fromValueToStr(value: Option[Int], maxLengthOfValue: Int): String = {
+      if (value.isEmpty) " " * maxLengthOfValue
+      else {
+        val startedString = value.get.toString
+        val diffOfMaxAndCurrentString = maxLengthOfValue - startedString.length
+        if (diffOfMaxAndCurrentString % 2 == 0) " " * (diffOfMaxAndCurrentString / 2) + startedString + " " * (diffOfMaxAndCurrentString / 2)
+        else " " + " " * (diffOfMaxAndCurrentString / 2) + startedString + " " * (diffOfMaxAndCurrentString / 2)
+      }
+    }
+
+    def merge2Arrays(arr1: Array[Option[Int]], arr2: Array[Option[Int]]): Array[Option[Int]] = {
+      val sizeOfRes = math.min(arr1.length, arr2.length)
+      val arrRes = Array.ofDim[Option[Int]](sizeOfRes)
+      (0 until sizeOfRes).foreach(index => arrRes(index) = index match {
+        case ind if arr1(ind).isDefined => arr1(ind)
+        case ind if arr2(ind).isDefined => arr2(ind)
+        case _ => None
+      })
+      arrRes
+    }
+
+    val arrayOfTree: Array[Option[Int]] = Array[Option[Int]]((0 until math.pow(2, depth).toInt - 1).map(_ => None): _*)
+    def bstToArray(root: BST, indexOfRoot: Int, currentArray: Array[Option[Int]]): Array[Option[Int]] = root match {
+      case head if head.left.isDefined && head.right.isDefined => merge2Arrays(bstToArray(head.left.get, indexOfRoot * 2 + 1, currentArray), bstToArray(head.right.get, indexOfRoot * 2 + 2, currentArray)).patch(indexOfRoot, Array(Some(head.value)), 1)
+      case head if head.left.isDefined => bstToArray(head.left.get, indexOfRoot * 2 + 1, currentArray).patch(indexOfRoot, Array(Some(head.value)), 1)
+      case head if head.right.isDefined => bstToArray(head.right.get, indexOfRoot * 2 + 2, currentArray).patch(indexOfRoot, Array(Some(head.value)), 1)
+      case head => currentArray.patch(indexOfRoot, Array(Some(head.value)), 1)
+    }
+
+    val graphAsArray = bstToArray(this, 0, arrayOfTree)
+
+    def array2StringAsTree(array: Array[Option[Int]], depth: Int, maxLength: Int, symbolsBetweenNodes: Int): String = {
+      val list = array.toList
+      val width = math.pow(2, depth).toInt * maxLength + symbolsBetweenNodes
+
+      def matchListOfElems(list: List[Option[Int]], currentString: String = "", numOfValuesAtCurrentLevel: Int = 1, currentPos: Int = 1, lenOfStringForValue: Int = width): String = list match {
+        case Nil => currentString
+        case head :: tail =>
+          if (currentPos != numOfValuesAtCurrentLevel) {
+            matchListOfElems(tail, currentString + fromValueToStr(head, lenOfStringForValue), numOfValuesAtCurrentLevel, currentPos + 1, lenOfStringForValue)
+          }
+          else {
+            val numOfValuesAtNextLevel = numOfValuesAtCurrentLevel * 2
+            val lenOfStringForNextLevel = width / numOfValuesAtNextLevel
+            matchListOfElems(tail, currentString + fromValueToStr(head, lenOfStringForValue) + "\n", numOfValuesAtNextLevel, 1, lenOfStringForNextLevel)
+          }
+      }
+      matchListOfElems(list)
+    }
+
+    array2StringAsTree(graphAsArray, depth, maxLength, 2)
   }
-  //override def toString()
 }
 
 object TreeTest extends App {
@@ -78,16 +131,14 @@ object TreeTest extends App {
   val markerItem3 = (Math.random() * maxValue).toInt
 
   // Generate huge tree
-  def generatror(acc: Int, bstTree: BST): BST = acc match {
-    case value if value == nodesCount => bstTree.add(Random.nextInt(maxValue))
-    case _ => generatror(acc + 1, bstTree.add(Random.nextInt(maxValue)))
+  def generatror(bstTree: BST, acc: Int = 0): BST = acc match {
+    case value if value == nodesCount - 1 => bstTree
+    case _ => generatror(bstTree.add(Random.nextInt(maxValue)), acc + 1)
   }
 
   val root: BST = BSTImpl(maxValue / 2)
-  val tree: BST = generatror(0, root)
+  val tree: BST = generatror(root)
 
-  println(tree.myToString())
-  println(tree)
   // add marker items
   val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
 
