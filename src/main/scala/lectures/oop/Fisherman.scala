024436daa51481fc51060bb528e2259d7c174407
Igor Titov <i.titov1@tinkoff.ru>
2017-12-08 10:54:14 +0300

    Rewrite without Objects and Print

--- src/main/scala/lectures/oop/Fisherman.scala
+++ src/main/scala/lectures/oop/Fisherman.scala
@@ -21,16 +21,16 @@ package lectures.oop
   * * * * так можно: PartD { def str = { val newStr = doSomethingWithD("D"); newStr }}
   * * * * содержимое FishermansApp менять нельзя
   **/
-object PartD {
-  def str = "D"
+trait PartD extends PartY {
+  override def str = super.str + "D"
 }
 
-object PartY {
+trait PartY {
   def str = "Y"
 }
 
-trait PartCH extends {
-  def str = "CH"
+trait PartCH extends PartD {
+  override def str = super.str + "CH"
 }
 
 trait PartK extends PartCH {
@@ -42,12 +42,14 @@ trait PartA extends PartK {
 }
 
 class PartO extends PartA {
-  override def str = PartY.str + PartD.str + "O" + super.str
+  override def str = {
+    val (start, finish) = super.str.splitAt(2)
+    start + "O" + finish
+  }
 }
 
 object FishermansApp extends App {
   val o = new PartO
   println(o.str)
 }
-
 
