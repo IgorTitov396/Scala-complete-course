7b027d570143f0ad289b0ede8fe750baad97c9f7
Igor Titov <titov@phystech.edu>
2018-01-14 22:24:43 +0300

    add impl and generator for Float and Strings

--- src/main/scala/lectures/oop/types/GeneralBST.scala
+++ src/main/scala/lectures/oop/types/GeneralBST.scala
@@ -1,5 +1,7 @@
 package lectures.oop.types
 
+import scala.math.Ordering
+
 /**
   * Модифицируйте реализацию BSTImpl из предыдущего задания.
   * Используя тайп параметры и паттерн Type Class, реализуйте GeneralBSTImpl таким образом,
@@ -13,23 +15,105 @@ package lectures.oop.types
   * * * * Watches из задачи SortStuff. Большими считаются часы с большей стоимостью
   */
 
-trait GeneralBST {
-  //val value: T
-  val left: Option[GeneralBST]
-  val right: Option[GeneralBST]
+trait GeneralBST[T] {
+  val value: T
+  val left: Option[GeneralBST[T]]
+  val right: Option[GeneralBST[T]]
 
-  def add(newValue: Int): GeneralBST
+  def add(newValue: T): GeneralBST[T]
 
-  def find(value: Int): Option[GeneralBST]
+  def find(value: T): Option[GeneralBST[T]]
 }
 
-class GeneralBSTImpl extends GeneralBST {
-  //override val value = _
 
-  override def find(value: Int): Option[GeneralBST] = ???
+case class GeneralBSTImpl[T](value: T,
+                         left: Option[GeneralBSTImpl[T]] = None,
+                         right: Option[GeneralBSTImpl[T]] = None)(implicit ord: Ordering[T]) extends GeneralBST[T] {
 
-  override def add(newValue: Int): GeneralBST = ???
+  override def add(newValue: T): GeneralBST[T] = {
+    if (ord.lt(newValue, this.value)) {
+      left.fold(
+        this.copy(
+          left = Some(GeneralBSTImpl(newValue))
+        )
+      )( bst_left =>
+        this.copy(
+          left = Some(bst_left.add(newValue).asInstanceOf[GeneralBSTImpl[T]])
+        )
+      )
+    }
+    else {
+      right.fold(
+        this.copy(
+          right = Some(GeneralBSTImpl(newValue))
+        )
+      )( bst_right =>
+        this.copy(
+          right = Some(bst_right.add(newValue).asInstanceOf[GeneralBSTImpl[T]])
+        )
+      )
+    }
+  }
 
-  override val left: Option[GeneralBST] = ???
-  override val right: Option[GeneralBST] = ???
+  override def find(value: T): Option[GeneralBST[T]] = {
+    if (ord.equiv(value, this.value)) Some(this)
+    else if (ord.gt(this.value, value)) left.flatMap(_.find(value))
+    else right.flatMap(_.find(value))
+  }
+}
+
+object FloatTreeTest extends App {
+
+  val sc = new java.util.Scanner(System.in)
+  val maxValue = 110000
+  val nodesCount = sc.nextInt()
+
+  val markerItem = (Math.random() * maxValue).toFloat
+  val markerItem2 = (Math.random() * maxValue).toFloat
+  val markerItem3 = (Math.random() * maxValue).toFloat
+
+  // Generate huge tree
+  def floatGeneratror(bstTree: GeneralBST[Float], acc: Int = 0): GeneralBST[Float] = acc match {
+    case value if value == nodesCount - 1 => bstTree
+    case _ => floatGeneratror(bstTree.add((Math.random() * maxValue).toFloat), acc + 1)
+  }
+
+  val root = GeneralBSTImpl[Float](maxValue / 2)
+  val tree = floatGeneratror(root)
+
+  // add marker items
+  val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
+
+  // check that search is correct
+  require(testTree.find(markerItem).map(_.value).contains(markerItem))
+  require(testTree.find(markerItem2).map(_.value).contains(markerItem2))
+  require(testTree.find(markerItem3).map(_.value).contains(markerItem3))
+}
+
+object StringTreeTest extends App {
+
+  val sc = new java.util.Scanner(System.in)
+  val maxValue = 110000
+  val nodesCount = sc.nextInt()
+
+  val markerItem = (Math.random() * maxValue).toInt.toString
+  val markerItem2 = (Math.random() * maxValue).toInt.toString
+  val markerItem3 = (Math.random() * maxValue).toInt.toString
+
+  // Generate huge tree
+  def floatGeneratror(bstTree: GeneralBST[String], acc: Int = 0): GeneralBST[String] = acc match {
+    case value if value == nodesCount - 1 => bstTree
+    case _ => floatGeneratror(bstTree.add((Math.random() * maxValue).toInt.toString), acc + 1)
+  }
+
+  val root = GeneralBSTImpl[String]((maxValue / 2).toString)
+  val tree = floatGeneratror(root)
+
+  // add marker items
+  val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
+
+  // check that search is correct
+  require(testTree.find(markerItem).map(_.value).contains(markerItem))
+  require(testTree.find(markerItem2).map(_.value).contains(markerItem2))
+  require(testTree.find(markerItem3).map(_.value).contains(markerItem3))
 }
